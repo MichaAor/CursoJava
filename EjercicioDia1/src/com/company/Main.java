63aff72bc597d03bb4262e5a4512bd906a75c087
Misael Flores <misaelflores4190@gmail.com>
2022-04-04 15:34:56 -0300

    Ejercicio 2 del dia

--- EjercicioDia1/src/com/company/Main.java
+++ EjercicioDia1/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-	    Triangle triangle = new Triangle(12,12,12);
-	    triangle.showType(triangle.typeTriangle());
+        ej1();
+	    ej2();
+    }
+
+    public static void ej1(){
+        Triangle triangle = new Triangle(12,12,12);
+        triangle.showType(triangle.typeTriangle());
+    }
+
+    public static void ej2(){
+        Scanner sc = new Scanner(System.in);
+        int nmb;
+        System.out.println("Enter a number from 1 to 14");
+        nmb = sc.nextInt();
+        if(nmb >=1 && nmb <=14){
+            switch (nmb){
+                case 2: case 3: case 5: case 7: case 11: case 13:
+                    System.out.println("Es numero primo");
+                    break;
+                case 4: case 6: case 8: case 9: case 10: case 12: case 14:
+                    System.out.println("No es primo");
+                    break;
+            }
+        }
     }
 }
