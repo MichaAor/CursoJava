ab0797c6dcec1434aa5015a67ffc946a46a04ad4
Misael Flores <misaelflores4190@gmail.com>
2022-04-05 19:40:58 -0300

    Finalizacion de la ejercitacion dia 2

--- Dia 2/EjerciciosDia2/src/com/company/Main.java
+++ Dia 2/EjerciciosDia2/src/com/company/Main.java
@@ -1,27 +1,23 @@
 package com.company;
 
+import com.company.ejInterfaces.Auto;
+import com.company.ejInterfaces.Perro;
 import com.company.ejPairProgramming.ejPairProgramming;
-
-import java.util.Arrays;
+import static com.company.ejArrays.ejArr.motrarEjArr;
 
 public class Main {
 
     public static void main(String[] args) {
+        //EJ1
+        motrarEjArr();
+        //EJ2
         ejPairProgramming ej = new ejPairProgramming();
         ej.ejecutarEj();
+        //EJ3
+        Auto auto = new Auto();
+        auto.metrosEnTiempo();
+        Perro perro = new Perro();
+        perro.metrosEnTiempo();
     }
 
-
-
-    public static void ordenAlf(String[] str){
-        Arrays.sort(str);
-        for(int i=0;i<str.length;i++){
-            System.out.println(str[i]);
-        }
-    }
-
-    public static void orden(String[] str){
-       /* String[] str = {"coder", "devplace", "personas", "curso", "alumnado"};
-        ordenAlf(str);*/
-    }
 }
--- Dia 2/EjerciciosDia2/src/com/company/ejArrays/ejArr.java
+++ Dia 2/EjerciciosDia2/src/com/company/ejArrays/ejArr.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class ejArr {
 
+    public static void motrarEjArr(){
+        String[] arr = {"coder", "devplace", "personas", "curso", "alumnado" };
+        ordenAlf(arr);
+        OrdenamientoLetras(arr);
+    }
+
     public static void ordenAlf(String[] str){
     Arrays.sort(str);
     for(int i=0;i<str.length;i++){
@@ -11,6 +17,20 @@ public class ejArr {
         }
     }
 
-    public static void orden(String[] str){
-    }
+    public static void OrdenamientoLetras(String[] arr){
+            int cartA, carB;
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j< arr.length;j++){
+                    cartA = arr[i].length();
+                    carB = arr[j].length();
+                    if(cartA<carB){
+                        String temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            System.out.println("Show odd numbers: " + Arrays.toString(arr));
+        }
+
 }
